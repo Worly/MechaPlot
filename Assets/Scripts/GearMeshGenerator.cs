3c8c7bf48637fa1abee7d8601abde64f4d988439
Valentino Vukelic <tino.vukelic@gmail.com>
2022-03-10 21:04:27 +0100

    Moved mesh generation code to new class MeshGenerator

--- Assets/Scripts/GearMeshGenerator.cs
+++ Assets/Scripts/GearMeshGenerator.cs
@@ -62,116 +62,50 @@ public class GearMeshGenerator : MonoBehaviour
 
         var innerRadius = innerCircumference / (2 * Mathf.PI);
 
-        var mesh = new Mesh();
-
         var circleVertexCount = (int)Mathf.Round(circumference / toothWidth);
 
-        var leftSideTriangles = GenerateSide(outerRadiusLeft, innerRadius, circleVertexCount, -thickness / 2f, -1, out List<Vertex> outerLeftCircle, out List<Vertex> innerLeftCircle);
-        var rightSideTriangles = GenerateSide(outerRadiusRight, innerRadius, circleVertexCount, thickness / 2f, 1, out List<Vertex> outerRightCircle, out List<Vertex> innerRightCircle);
+        var meshBuilder = new MeshBuilder();
+
+        GenerateSide(meshBuilder, outerRadiusLeft, innerRadius, circleVertexCount, -thickness / 2f, -1, out List<Vertex> outerLeftCircle, out List<Vertex> innerLeftCircle);
+        GenerateSide(meshBuilder, outerRadiusRight, innerRadius, circleVertexCount, thickness / 2f, 1, out List<Vertex> outerRightCircle, out List<Vertex> innerRightCircle);
 
         innerLeftCircle.Reverse(1, innerLeftCircle.Count - 1);
         outerRightCircle.Reverse(1, outerRightCircle.Count - 1);
 
-        var innerBridgeTriangles = GenerateCircleBridge(innerRightCircle, innerLeftCircle, copyVertices: true);
+        meshBuilder.MakeCircleBridge(innerRightCircle, innerLeftCircle, copyVertices: true);
 
-        List<Triangle> outerBridgeTriangles;
         if (circleVertexCount % 2 == 0)
-            outerBridgeTriangles = GenerateTeeth(outerRightCircle, outerLeftCircle);
+            GenerateTeeth(meshBuilder, outerRightCircle, outerLeftCircle);
         else
-            outerBridgeTriangles = GenerateCircleBridge(outerRightCircle, outerLeftCircle, copyVertices: true);
+            meshBuilder.MakeCircleBridge(outerRightCircle, outerLeftCircle, copyVertices: true);
 
-        var meshBuilder = new MeshBuilder();
-        meshBuilder
-            .AddTriangles(leftSideTriangles)
-            .AddTriangles(rightSideTriangles)
-            .AddTriangles(innerBridgeTriangles)
-            .AddTriangles(outerBridgeTriangles)
-            .Build(out Vector3[] vertices, out int[] triangles, out Vector3[] normals);
-
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
-        mesh.normals = normals;
-
-        meshFilter.mesh = mesh;
+        meshFilter.mesh = meshBuilder.Build();
     }
 
-    private List<Triangle> GenerateSide(float outerRadius, float innerRadius, int vertexCount, float zValue, int zNormalDirection, out List<Vertex> outerCircle, out List<Vertex> innerCircle)
+    private void GenerateSide(MeshBuilder meshBuilder, float outerRadius, float innerRadius, int vertexCount, float zValue, int zNormalDirection, out List<Vertex> outerCircle, out List<Vertex> innerCircle)
     {
-        outerCircle = GenerateCircle(vertexCount, outerRadius, zValue, direction: -zNormalDirection);
+        outerCircle = meshBuilder.GenerateCircle(vertexCount, outerRadius, zValue, direction: -zNormalDirection);
 
-        innerCircle = GenerateCircle(vertexCount, innerRadius, zValue, direction: -zNormalDirection);
+        innerCircle = meshBuilder.GenerateCircle(vertexCount, innerRadius, zValue, direction: -zNormalDirection);
 
-        return GenerateCircleBridge(outerCircle, innerCircle, copyVertices: false);
+        meshBuilder.MakeCircleBridge(outerCircle, innerCircle, copyVertices: false);
     }
 
-    private List<Vertex> GenerateCircle(int vertexCount, float radius, float zValue, int direction)
-    {
-        var result = new List<Vertex>();
-
-        for (int i = 0; i < vertexCount; i++)
-        {
-            var angle = direction * 2 * Mathf.PI * i / vertexCount;
-
-            var vertex = new Vertex
-            (
-                x: Mathf.Sin(angle) * radius,
-                y: Mathf.Cos(angle) * radius,
-                z: zValue
-            );
-            result.Add(vertex);
-        }
-
-        return result;
-    }
-
-    private List<Triangle> GenerateCircleBridge(List<Vertex> circle1, List<Vertex> circle2, bool copyVertices)
+    private void GenerateTeeth(MeshBuilder meshBuilder, List<Vertex> circle1, List<Vertex> circle2)
     {
         if (circle1.Count != circle2.Count)
             throw new ArgumentException("Circle1 vertex count must be same as circle2 vertex count");
 
-        var triangles = new List<Triangle>();
-
-        for (int i = 0; i < circle1.Count; i++)
-        {
-            var quadVertices = new List<Vertex>()
-            {
-                circle1[i],
-                circle1[(i + 1) % circle1.Count],
-                circle2[(i + 1) % circle1.Count],
-                circle2[i]
-            };
-
-            if (copyVertices)
-                quadVertices = quadVertices.Select(o => new Vertex(o.Vector3)).ToList();
-
-            triangles.AddRange(MakeQuad(quadVertices));
-        }
-
-        return triangles;
-    }
-
-    private List<Triangle> GenerateTeeth(List<Vertex> circle1, List<Vertex> circle2)
-    {
-        if (circle1.Count != circle2.Count)
-            throw new ArgumentException("Circle1 vertex count must be same as circle2 vertex count");
-
-        var triangles = new List<Triangle>();
-
-        var newCircle1Vertices = GenerateTeethVertices(circle1, out List<Triangle> triangles1);
-        var newCircle2Vertices = GenerateTeethVertices(circle2, out List<Triangle> triangles2);
+        var newCircle1Vertices = GenerateTeethVertices(meshBuilder, circle1, out List<Triangle> triangles1);
+        var newCircle2Vertices = GenerateTeethVertices(meshBuilder, circle2, out _);
 
         foreach (var triangle in triangles1)
             triangle.Flip();
 
-        triangles.AddRange(triangles1);
-        triangles.AddRange(triangles2);
-
-        triangles.AddRange(GenerateCircleBridge(newCircle1Vertices, newCircle2Vertices, copyVertices: true));
-
-        return triangles;
+        meshBuilder.MakeCircleBridge(newCircle1Vertices, newCircle2Vertices, copyVertices: true);
     }
 
-    private List<Vertex> GenerateTeethVertices(List<Vertex> circle, out List<Triangle> triangles)
+    private List<Vertex> GenerateTeethVertices(MeshBuilder meshBuilder, List<Vertex> circle, out List<Triangle> triangles)
     {
         var newVertices = new List<Vertex>();
 
@@ -207,106 +141,12 @@ public class GearMeshGenerator : MonoBehaviour
 
                 newVertices.Add(sixthVertex);
 
-                triangles.AddRange(MakeQuad(new List<Vertex>() { secondVertex, thirdVertex, fourthVector, fifthVertex }));
-                triangles.AddRange(MakeQuad(new List<Vertex>() { firstVertex, secondVertex, fifthVertex, sixthVertex }));
+                triangles.AddRange(meshBuilder.MakeQuad(new List<Vertex>() { secondVertex, thirdVertex, fourthVector, fifthVertex }));
+                triangles.AddRange(meshBuilder.MakeQuad(new List<Vertex>() { firstVertex, secondVertex, fifthVertex, sixthVertex }));
             }
         }
 
         return newVertices;
-    }
-
-    private List<Triangle> MakeQuad(List<Vertex> vertices)
-    {
-        if (vertices.Count != 4)
-            throw new ArgumentException("Quad must have exactly 4 vertices");
-
-        var triangles = new List<Triangle>();
-
-        triangles.Add(new Triangle(vertices[0], vertices[1], vertices[2]));
-        triangles.Add(new Triangle(vertices[2], vertices[3], vertices[0]));
-
-        return triangles;
-    }
-
-    private class Vertex
-    {
-        public Vector3 Vector3 { get; set; }
-        public Vector3 Normal { get; set; } = new Vector3(0, 0, 0);
-
-        public Vertex(Vector3 vector3)
-        {
-            Vector3 = vector3;
-        }
-
-        public Vertex(float x, float y, float z)
-        {
-            Vector3 = new Vector3(x, y, z);
-        }
-    }
-
-    private class Triangle
-    {
-        public Vertex[] Vertices { get; set; } = new Vertex[3];
-
-        public Triangle(Vertex v0, Vertex v1, Vertex v2)
-        {
-            Vertices[0] = v0;
-            Vertices[1] = v1;
-            Vertices[2] = v2;
-
-            GenerateNormals();
-        }
-
-        public void Flip()
-        {
-            var temp = Vertices[2];
-            Vertices[2] = Vertices[0];
-            Vertices[0] = temp;
-
-            GenerateNormals();
-        }
-
-        private void GenerateNormals()
-        {
-            var vector1 = Vertices[1].Vector3 - Vertices[0].Vector3;
-            var vector2 = Vertices[2].Vector3 - Vertices[0].Vector3;
-
-            var normal = Vector3.Cross(vector1, vector2);
-
-            Vertices[0].Normal = Vertices[1].Normal = Vertices[2].Normal = normal;
-        }
-    }
-
-    private class MeshBuilder
-    {
-        public HashSet<Triangle> triangles = new HashSet<Triangle>();
-
-        public MeshBuilder AddTriangles(IEnumerable<Triangle> triangles)
-        {
-            foreach (var triangle in triangles)
-                this.triangles.Add(triangle);
-
-            return this;
-        }
-
-        public void Build(out Vector3[] vertices, out int[] triangles, out Vector3[] normals)
-        {
-            var vertexSet = new HashSet<Vertex>();
-
-            foreach (var triangle in this.triangles)
-            {
-                vertexSet.Add(triangle.Vertices[0]);
-                vertexSet.Add(triangle.Vertices[1]);
-                vertexSet.Add(triangle.Vertices[2]);
-            }
-
-            var vertexList = vertexSet.ToList();
-
-            vertices = vertexList.Select(o => o.Vector3).ToArray();
-            normals = vertexList.Select(o => o.Normal).ToArray();
-
-            triangles = this.triangles.SelectMany(t => t.Vertices.Select(v => vertexList.IndexOf(v))).ToArray();
-        }
     }
 }
 
---
+++ Assets/Scripts/Utils/MeshBuilder.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class MeshBuilder
+{
+    private HashSet<Triangle> triangles = new HashSet<Triangle>();
+
+
+    public MeshBuilder()
+    {
+
+    }
+
+    public void AddTriangles(IEnumerable<Triangle> triangles)
+    {
+        foreach (var triangle in triangles)
+            this.triangles.Add(triangle);
+    }
+
+    public List<Vertex> GenerateCircle(int vertexCount, float radius, float zValue, int direction)
+    {
+        var result = new List<Vertex>();
+
+        for (int i = 0; i < vertexCount; i++)
+        {
+            var angle = direction * 2 * Mathf.PI * i / vertexCount;
+
+            var vertex = new Vertex
+            (
+                x: Mathf.Sin(angle) * radius,
+                y: Mathf.Cos(angle) * radius,
+                z: zValue
+            );
+            result.Add(vertex);
+        }
+
+        return result;
+    }
+
+    public List<Triangle> MakeCircleBridge(List<Vertex> circle1, List<Vertex> circle2, bool copyVertices)
+    {
+        if (circle1.Count != circle2.Count)
+            throw new ArgumentException("Circle1 vertex count must be same as circle2 vertex count");
+
+        var triangles = new List<Triangle>();
+
+        for (int i = 0; i < circle1.Count; i++)
+        {
+            var quadVertices = new List<Vertex>()
+            {
+                circle1[i],
+                circle1[(i + 1) % circle1.Count],
+                circle2[(i + 1) % circle1.Count],
+                circle2[i]
+            };
+
+            if (copyVertices)
+                quadVertices = quadVertices.Select(o => new Vertex(o.Vector3)).ToList();
+
+            triangles.AddRange(MakeQuad(quadVertices));
+        }
+
+        AddTriangles(triangles);
+
+        return triangles;
+    }
+
+    public List<Triangle> MakeQuad(List<Vertex> vertices)
+    {
+        if (vertices.Count != 4)
+            throw new ArgumentException("Quad must have exactly 4 vertices");
+
+        var triangles = new List<Triangle>();
+
+        triangles.Add(new Triangle(vertices[0], vertices[1], vertices[2]));
+        triangles.Add(new Triangle(vertices[2], vertices[3], vertices[0]));
+
+        AddTriangles(triangles);
+
+        return triangles;
+    }
+
+    public Mesh Build()
+    {
+        var mesh = new Mesh();
+
+        var vertexSet = new HashSet<Vertex>();
+
+        foreach (var triangle in this.triangles)
+        {
+            vertexSet.Add(triangle.Vertices[0]);
+            vertexSet.Add(triangle.Vertices[1]);
+            vertexSet.Add(triangle.Vertices[2]);
+        }
+
+        var vertexList = vertexSet.ToList();
+
+        mesh.vertices = vertexList.Select(o => o.Vector3).ToArray();
+        mesh.normals = vertexList.Select(o => o.Normal).ToArray();
+
+        mesh.triangles = this.triangles.SelectMany(t => t.Vertices.Select(v => vertexList.IndexOf(v))).ToArray();
+
+        mesh.name = "Generated mesh";
+        //mesh.Optimize();
+        return mesh;
+    }
+}
+
+public class Vertex
+{
+    public Vector3 Vector3 { get; set; }
+    public Vector3 Normal { get; set; } = new Vector3(0, 0, 0);
+
+    public Vertex(Vector3 vector3)
+    {
+        Vector3 = vector3;
+    }
+
+    public Vertex(float x, float y, float z)
+    {
+        Vector3 = new Vector3(x, y, z);
+    }
+}
+
+public class Triangle
+{
+    public Vertex[] Vertices { get; set; } = new Vertex[3];
+
+    public Triangle(Vertex v0, Vertex v1, Vertex v2)
+    {
+        Vertices[0] = v0;
+        Vertices[1] = v1;
+        Vertices[2] = v2;
+
+        GenerateNormals();
+    }
+
+    public void Flip()
+    {
+        var temp = Vertices[2];
+        Vertices[2] = Vertices[0];
+        Vertices[0] = temp;
+
+        GenerateNormals();
+    }
+
+    private void GenerateNormals()
+    {
+        var vector1 = Vertices[1].Vector3 - Vertices[0].Vector3;
+        var vector2 = Vertices[2].Vector3 - Vertices[0].Vector3;
+
+        var normal = Vector3.Cross(vector1, vector2);
+
+        Vertices[0].Normal = Vertices[1].Normal = Vertices[2].Normal = normal;
+    }
+}
