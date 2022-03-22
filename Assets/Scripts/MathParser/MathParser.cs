aee7213459300fd44026c950618ddf8ad44f8698
Valentino Vukelic <tino.vukelic@gmail.com>
2022-03-22 14:47:53 +0100

    Add MathParser

---
+++ Assets/Scripts/MathParser/MathParser.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace MathParser
+{
+    internal enum Operator
+    {
+        LEFT_BRACKET, RIGHT_BRACKET, PLUS, MINUS, TIMES, DIVIDED_BY, POWER, UNARY_MINUS, UNARY_PLUS
+    }
+
+    public class Parser
+    {
+        public static Node Parse(string expression)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == ' ')
+                    continue;
+
+                stringBuilder.Append(expression[i]);
+            }
+
+            var sanitizedExpression = stringBuilder.ToString();
+
+            var parser = new Parser(sanitizedExpression);
+
+            return parser.ParseExpression();
+        }
+
+        private Parser(string expression)
+        {
+            this.expression = expression;
+        }
+
+        private string expression;
+        private int index = 0;
+
+        // E -> T+E | T-E | T
+        private Node ParseExpression()
+        {
+            var left = ParseTerm();
+
+            if (Match("-"))
+            {
+                var right = ParseExpression();
+                return new OperationNode(Operation.SUBTRACTION, left, right);
+            }
+
+            if (Match("+"))
+            {
+                var right = ParseExpression();
+                return new OperationNode(Operation.ADDITION, left, right);
+            }
+
+            return left;
+        }
+
+        // T -> P*T | P/T | P
+        private Node ParseTerm()
+        {
+            var left = ParsePower();
+
+            if (Match("*"))
+            {
+                var right = ParseTerm();
+
+                return new OperationNode(Operation.MULTIPLICATION, left, right);
+            }
+
+            if (Match("/"))
+            {
+                var right = ParseTerm();
+
+                return new OperationNode(Operation.DIVISION, left, right);
+            }
+
+            return left;
+        }
+
+        // P -> F^P | F
+        private Node ParsePower()
+        {
+            var left = ParseFactor();
+
+            if (Match("^"))
+            {
+                var right = ParsePower();
+
+                return new OperationNode(Operation.POWER, left, right);
+            }
+
+            return left;
+        }
+
+        // F -> -F | +F | (E) | a
+        private Node ParseFactor()
+        {
+            var next = GetNext(true);
+
+            if (next.Value == "-")
+            {
+                var node = ParseFactor();
+                return new UnaryOperationNode(UnaryOperation.NEGATIVE, node);
+            }
+
+            if (next.Value == "+")
+            {
+                var node = ParseFactor();
+                return node;
+            }
+
+            if (next.Value == "(")
+            {
+                var node = ParseExpression();
+                if (!Match(")"))
+                    throw new SyntaxException("Missing closing brackets!");
+
+                return node;
+            }
+
+            if (next.Type == UnitType.ARGUMENT)
+            {
+                if (next.Value == "x")
+                    return new InputNode();
+                else
+                    return new ValueNode(next.Value);
+            }
+
+
+            throw new SyntaxException("Expected +, -, ( or argument!");
+        }
+
+        private bool Match(string unit)
+        {
+            if (index >= expression.Length)
+                return false;
+
+            var next = GetNext();
+            if (unit == next.Value)
+            {
+                GetNext(true);
+                return true;
+            }
+
+            return false;
+        }
+
+        private Unit GetNext(bool consume = false)
+        {
+            var regex = new Regex(@"\(|\)|[0-9]+(\.[0-9]*)?|x|\+|\-|\*|\/|\^");
+
+            var match = regex.Match(expression, index);
+
+            if (match.Success == false || match.Index != index)
+                throw new LexicalErrorException(expression);
+
+            if (consume)
+                index = match.Index + match.Length;
+
+            UnitType unitType;
+            if (char.IsNumber(match.Value[0]) || match.Value == "x")
+                unitType = UnitType.ARGUMENT;
+            else
+                unitType = UnitType.OPERATOR;
+
+            return new Unit()
+            {
+                Type = unitType,
+                Value = match.Value
+            };
+        }
+
+        private class Unit
+        {
+            public string Value { get; set; }
+            public UnitType Type { get; set; }
+        }
+
+        private enum UnitType
+        {
+            ARGUMENT,
+            OPERATOR
+        }
+    }
+
+    public class LexicalErrorException : Exception
+    {
+        public string Expression { get; set; }
+        public int Offset { get; set; }
+
+        public LexicalErrorException(string expression) : base("Uknown character at " + expression)
+        {
+            Expression = expression;
+        }
+    }
+
+    public class SyntaxException : Exception
+    {
+        public SyntaxException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/MathParser/Node.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MathParser
+{
+    public abstract class Node
+    {
+        public virtual Node Evaluate()
+        {
+            return this;
+        }
+    }
+
+    public class ValueNode : Node
+    {
+        public float Value { get; set; }
+
+        public ValueNode(string value)
+        {
+            this.Value = float.Parse(value);
+        }
+
+        public ValueNode(float value)
+        {
+            this.Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+
+    public class InputNode : Node
+    {
+        public override string ToString()
+        {
+            return "x";
+        }
+    }
+
+    public class OperationNode : Node
+    {
+        public Operation Operation { get; set; }
+
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        public OperationNode(Operation op, Node left, Node right)
+        {
+            Operation = op;
+            this.Left = left;
+            this.Right = right;
+        }
+
+        public override string ToString()
+        {
+            switch(Operation)
+            {
+                case Operation.POWER:
+                    return $"({Left}^{Right})";
+                case Operation.MULTIPLICATION:
+                    return $"({Left} * {Right})";
+                case Operation.DIVISION:
+                    return $"({Left} / {Right})";
+                case Operation.ADDITION:
+                    return $"({Left} + {Right})";
+                case Operation.SUBTRACTION:
+                    return $"({Left} - {Right})";
+                default:
+                    return "null";
+            }
+        }
+
+        public override Node Evaluate()
+        {
+            var left = Left.Evaluate();
+            var right = Right.Evaluate();
+
+            var leftValue = left as ValueNode;
+            var rightValue = right as ValueNode;
+
+            if (leftValue == null || rightValue == null)
+                return new OperationNode(Operation, left, right);
+
+            switch (Operation)
+            {
+                case Operation.POWER:
+                    return new ValueNode(Mathf.Pow(leftValue.Value, rightValue.Value));
+                case Operation.MULTIPLICATION:
+                    return new ValueNode(leftValue.Value * rightValue.Value);
+                case Operation.DIVISION:
+                    return new ValueNode(leftValue.Value / rightValue.Value);
+                case Operation.ADDITION:
+                    return new ValueNode(leftValue.Value + rightValue.Value);
+                case Operation.SUBTRACTION:
+                    return new ValueNode(leftValue.Value - rightValue.Value);
+                default:
+                    return null;
+            }
+        }
+    }
+
+    public class UnaryOperationNode : Node
+    {
+        public UnaryOperation Operation { get; set; }
+
+        public Node Node { get; set; }
+
+        public UnaryOperationNode(UnaryOperation unaryOperation, Node node)
+        {
+            Operation = unaryOperation;
+            Node = node;
+        }
+
+        public override string ToString()
+        {
+            return $"-{Node}";
+        }
+
+        public override Node Evaluate()
+        {
+            var node = Node.Evaluate();
+
+            var valueNode = node as ValueNode;
+
+            if (valueNode == null)
+                return new UnaryOperationNode(Operation, node);
+            else
+                return new ValueNode(-valueNode.Value);
+        }
+    }
+
+    public enum Operation
+    {
+        ADDITION,
+        SUBTRACTION,
+        MULTIPLICATION,
+        DIVISION,
+        POWER
+    }
+
+    public enum UnaryOperation
+    {
+        NEGATIVE,
+    }
+}
